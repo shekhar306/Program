ee7d120bff42bd8381d5bfc405657e64a8a7ad90
shekhar306 <56515608+shekhar306@users.noreply.github.com>
2020-11-23 12:29:09 +0530

    Update Transpose of a 2D matrix.py

--- Transpose of a 2D matrix.py
+++ Transpose of a 2D matrix.py
@@ -1,8 +1,7 @@
-A=[[1][2],[4][3],[5][6]]
-B=[]
-for i in range(len(A)):
-   for j in range(len(A[0])):
-       B[j][i] = A[i][j]
-
-for b in B:
-   print(b)
+m = [[1,2],[3,4],[5,6]] 
+for row in m : 
+    print(row) 
+rez = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))] 
+print("\n") 
+for row in rez: 
+    print(row)
